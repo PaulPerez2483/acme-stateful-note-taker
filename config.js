8aa5dc80b0761e894970c431b38cce0f1d703f72
Paul Perez <colopaul@hotmail.com>
2019-10-28 21:46:38 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const root = document.getElementById('root');
+
+const { Component } = React;
+const { HashRouter, Route, Link, Switch } = ReactRouterDOM;
+
+class App extends Component {
+    constructor() {
+        super();
+        // this.state = {
+
+        // }
+    }
+
+    render() {
+
+    }
+}
+
+ReactDOM.render(<App />, root)
